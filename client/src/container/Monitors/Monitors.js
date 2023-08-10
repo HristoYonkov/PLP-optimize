e5e903cb3a16ad984cf8f590143e93c52cd1a21a
Hristo Yonkov <hristopturs@gmail.com>
2023-08-10 14:38:35 +0300

    update monitors according to new code changes!

--- client/src/container/Monitors/Monitors.js
+++ client/src/container/Monitors/Monitors.js
@@ -7,22 +7,50 @@ import FilterMobile from '../../components/FilterMobile/FilterMobile'
 import { calcMinMaxPrice } from '../../hooks/calcMinMaxPrice'
 
 
-const Monitors = ({ state, setCurrentState, originalState, setBackupProducts, setBuyedProducts }) => {
+const Monitors = ({ state, setCurrentState, setBuyedProducts }) => {
     const [products, setProducts] = useState(state);
     const [interval, setInterval] = useState(4);
+    // Filtering states..
+    const [selected, setSelected] = useState([]);
+    const [filteredPrice, setFilteredPrice] = useState({
+        price: calcMinMaxPrice(state).min
+    });
+    const [colors, setColors] = useState({
+        white: false,
+        black: false,
+        blue: false
+    });
+    // Sorting state..
+    const [sortValue, setSortValue] = useState('');
+
 
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
+        setInterval(4);
     }, []);
 
     useEffect(() => {
-        setProducts(state);
-        setInterval(4);
     }, [state]);
 
     useEffect(() => {
-        setProducts((curr) => [...curr, ...state.slice(curr.length, curr.length + interval)]);
-    }, [interval, state]);
+        // filter products!!!
+        setProducts(state);
+        if (selected.length > 0) {
+            setProducts(state => state.filter((x) => selected.includes(x.color)));
+        }
+        setProducts(state => state.filter((x) => x.price >= filteredPrice.price));
+        // Sort products!!!
+        if (sortValue === 'priceAsc') {
+            setProducts(state => [...state.sort((a, b) => a.price - b.price)]);
+        } else if (sortValue === 'priceDesc') {
+            setProducts(state => [...state.sort((a, b) => b.price - a.price)]);
+        } else if (sortValue === 'alphaA-Z') {
+            setProducts(state => [...state.sort((a, b) => a.name.localeCompare(b.name))]);
+        } else if (sortValue === 'alphaZ-A') {
+            setProducts(state => [...state.sort((a, b) => b.name.localeCompare(a.name))]);
+        }
+
+    }, [interval, selected, filteredPrice, state, sortValue]);
 
     const loadMoreHandler = () => {
         setInterval(state => state + 4);
@@ -31,7 +59,14 @@ const Monitors = ({ state, setCurrentState, originalState, setBackupProducts, se
 
     return (
         <div className='wrapper'>
-            <Filter minMaxPrice={calcMinMaxPrice(originalState)} originalState={originalState} setCurrentState={setCurrentState} />
+            <Filter
+                minMaxPrice={calcMinMaxPrice(state)}
+                setSelected={setSelected}
+                setFilteredPrice={setFilteredPrice}
+                filteredPrice={filteredPrice}
+                colors={colors}
+                setColors={setColors}
+            />
 
             <div className='app__container'>
                 <section className='app__container-top'>
@@ -49,18 +84,25 @@ const Monitors = ({ state, setCurrentState, originalState, setBackupProducts, se
 
                     <div className='app__container-sort'>
                         <div>
-                            <FilterMobile minMaxPrice={calcMinMaxPrice(originalState)} originalState={originalState} setCurrentState={setCurrentState} />
+                            <FilterMobile
+                                minMaxPrice={calcMinMaxPrice(state)}
+                                setSelected={setSelected}
+                                setFilteredPrice={setFilteredPrice}
+                                filteredPrice={filteredPrice}
+                                colors={colors}
+                                setColors={setColors}
+                            />
                         </div>
 
                         <div>
-                            <Dropdown setProducts={setProducts} reset={state} />
+                            <Dropdown sortValue={sortValue} setSortValue={setSortValue} />
                         </div>
                     </div>
                 </section>
 
                 <section className='app__container-products'>
                     {products.slice(0, interval).map((item) =>
-                        <Card key={item.id} item={item} setBackupProducts={setBackupProducts} setBuyedProducts={setBuyedProducts} />
+                        <Card key={item.id} item={item} setBackupProducts={setCurrentState} setBuyedProducts={setBuyedProducts} />
                     )}
                 </section>
                 {interval < products.length &&
